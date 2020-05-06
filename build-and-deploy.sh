ba257480bff8dde4c9024f681be7f686b7725b97
Arlan Jaska <ajaska@berkeley.edu>
2020-05-06 13:05:42 -0700

    oops

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,3 +3,6 @@ set -euox pipefail
 
 rm -r ./docs || true
 parcel build --public-url 'https://www.ajaska.com/experiments/'  --dist-dir docs --no-scope-hoist src/index.html 
+
+git add ./docs
+git commit -m "Re-build"
