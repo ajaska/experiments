0b6e3c4819d959517facaad29b957b742a5e8fd2
Arlan Jaska <ajaska@berkeley.edu>
2020-05-09 11:50:47 -0700

    Move things around a little

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,7 +2,7 @@
 set -euox pipefail
 
 rm -r ./docs || true
-parcel build --public-url 'https://www.ajaska.com/experiments/'  --dist-dir docs --no-scope-hoist src/index.html src/font-anim/index.html
+parcel build --public-url 'https://www.ajaska.com/experiments/'  --dist-dir docs --no-scope-hoist src/index.html src/**/index.html
 
 git add ./docs
 git commit -m "Re-build"
