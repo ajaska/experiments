89ada1fe8226a077299d3057b1724c852f6a5943
Arlan Jaska <ajaska@berkeley.edu>
2020-07-29 13:22:26 -0700

    Flip lightswitch lol

--- src/lightswitch/index.tsx
+++ src/lightswitch/index.tsx
@@ -4,19 +4,19 @@ import * as ReactDOM from "react-dom";
 import classNames from "classnames";
 
 const App = () => {
-  const [lights, setLights] = React.useState(true);
+  const [lights, setLights] = React.useState(false);
 
   return (
     <div className={classNames("container", { "lights-out": !lights })}>
       <div className="switch-box">
-        <div className={classNames("toggle-switch-holder", { off: !lights })}>
+        <div className={classNames("toggle-switch-holder", { off: lights })}>
           <div
             className="toggle-switch-top"
-            onClick={() => (lights ? setLights(false) : null)}
+            onClick={() => (lights ? null : setLights(true))}
           />
           <div
             className="toggle-switch-bottom"
-            onClick={() => (lights ? null : setLights(true))}
+            onClick={() => (lights ? setLights(false) : null)}
           />
         </div>
       </div>
