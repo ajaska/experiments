9d02ab8e8be9901794b8622facca257ec977d186
Arlan Jaska <ajaska@berkeley.edu>
2020-07-29 15:18:03 -0700

    weebsockets

--- src/lightswitch/index.tsx
+++ src/lightswitch/index.tsx
@@ -3,11 +3,60 @@ import * as ReactDOM from "react-dom";
 
 import classNames from "classnames";
 
+interface Data {
+  on: boolean;
+}
+
+declare var process: any;
+const ws_url =
+  process.env.NODE_ENV === "development"
+    ? "ws://localhost:1234"
+    : "wss://experiments-do.ajaska.com:443/lightswitch/";
+
 const App = () => {
-  const [lights, setLights] = React.useState(false);
+  const [lights, _setLights] = React.useState(false);
+  const [websocket, setWebsocket] = React.useState<WebSocket | null>(null);
+  const [websocketError, setWebsocketError] = React.useState(false);
+
+  React.useEffect(() => {
+    const websocket = new WebSocket(ws_url);
+    websocket.onmessage = (e) => {
+      const data: Data = JSON.parse(e.data);
+      setLights(data.on);
+    };
+
+    websocket.onerror = () => {
+      console.log("falling back to singleplayer mode");
+      setWebsocket(null);
+      setWebsocketError(true);
+    };
+
+    setWebsocket(websocket);
+
+    return () => {
+      websocket.close();
+    };
+  }, []);
+
+  const setLights = React.useCallback(
+    (lights: boolean) => {
+      _setLights(lights);
+      if (websocket != null) {
+        websocket.send(JSON.stringify({ on: lights }));
+      }
+    },
+    [websocket]
+  );
+
+  if (!websocket && !websocketError) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className={classNames("container", { "lights-out": !lights })}>
+      {websocketError ? (
+        <div style={{ opacity: 0.6 }}>(error connecting to server)</div>
+      ) : null}
       <div className="switch-box">
         <div className={classNames("toggle-switch-holder", { off: lights })}>
           <div
