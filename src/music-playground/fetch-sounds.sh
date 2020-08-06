baed04cf64add852c97e850329f98ddffe945843
Arlan Jaska <ajaska@berkeley.edu>
2020-08-06 15:41:50 -0700

    Grab mp4 versions as well

--- src/music-playground/fetch-sounds.sh
+++ src/music-playground/fetch-sounds.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -x
+
 while read p; do
-  curl https://learningmusic.ableton.com/lessons/sounds/"$p".ogg -O --create-dirs --output ./"$p".ogg
+  curl -O --create-dirs --output ./"$p".ogg https://learningmusic.ableton.com/lessons/sounds/"$p".ogg 
+  curl -O --create-dirs --output ./"$p".mp4 https://learningmusic.ableton.com/lessons/sounds/"$p".mp4 
 done <sound_files.txt
 
